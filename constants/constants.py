9ac2d97513a57aa6b9a6afa9a25e729f829a913e
Gift Anyaoma <51029157+Anyaoma@users.noreply.github.com>
2025-09-08 00:05:42 +0100

    Add files via upload

---
+++ constants/constants.py
@@ -0,0 +1,8 @@
+api_hist_url = "https://data.alpaca.markets"
+api_ord_url = "https://paper-api.alpaca.markets"
+api_key = "PKCPB0BAZO2WDD5XY81J"
+api_secret = "8RaN8l4eCGVEeHsGmCYIdeDzQr5Plyi7vEeN6i3P"
+SECURE_HEADER = {
+    "accept": "application/json",
+    "APCA-API-KEY-ID": "PKCPB0BAZO2WDD5XY81J",
+    "APCA-API-SECRET-KEY": "8RaN8l4eCGVEeHsGmCYIdeDzQr5Plyi7vEeN6i3P"}
